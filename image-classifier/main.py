18a952deb9cbdd2b13274cc416f8e95cc49de550
Tomislav Dukez <tomdu3@ymail.com>
2025-05-21 08:15:01 +0100

    add initial setup for image classifier project

---
+++ image-classifier/main.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import (
+    MobileNetV2,
+    preprocess_input,
+    decode_predictions
+)
+from PIL import Image
+
